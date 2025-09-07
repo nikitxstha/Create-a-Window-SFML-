818550df9a106d7089306d689d9504f8f6c94ba3
Nikit Shrestha <nikitxsthacoding@gmail.com>
2025-09-07 14:36:03 +0545

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<SFML/Graphics.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	//Create Window
+	RenderWindow window(VideoMode({ 1200,800 }), "SpawnBalls");
+
+	//Infinity Loop to display window unless closed
+	while (window.isOpen()) {
+
+		//Loop to handle events like Closing Window/Resizing Window/Taking Keyboard/Mouse Input
+		while (optional event = window.pollEvent()) {
+
+			//To close window
+			if (event->is<Event::Closed>()) {
+				window.close();
+			}
+		}
+		//Set Background Color
+		window.clear(Color::Black);
+
+		//Display Window
+		window.display();
+	}
+}
